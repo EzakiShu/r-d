bfd521ac25b4d0ab37535819447dcef6bcd8fa49
ShuEzaki <ezashu0227@gmail.com>
2021-11-17 20:10:38 +0900

    thread on

--- people_detection/people_detection.py
+++ people_detection/people_detection.py
@@ -64,4 +64,4 @@ if __name__ == "__main__":
     model = YOLO(**vars(Namespace))
 
     # サーバーの起動
-    app.run(debug=False, host='0.0.0.0', port=8081)
+    app.run(debug=False, host='0.0.0.0', port=8081,threaded=True)
