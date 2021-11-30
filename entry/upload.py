91201ff831a75eb21c0af6e11ed0d8283b946b01
ShuEzaki <ezashu0227@gmail.com>
2021-11-30 20:41:40 +0900

    fix url

--- entry/upload.py
+++ entry/upload.py
@@ -22,15 +22,15 @@ def uploads_file():
     img_b = i2b(img)
 
     #画像の送信
-    url = "http://python-detection3:8080/api/predict"
+    url = "http://python-detection1:8080/api/predict"
     img_data = {
         "data":img_b
     }
     response = requests.post(url,data=img_data)
     #return jsonify(response.json())
     img = '<img src="data:image/png;base64,' + response.json()['data'] + '"/>'
-    #time = response.json()['time']
-    return img
+    time = response.json()['time']
+    return img, time
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=8080,threaded=True)
