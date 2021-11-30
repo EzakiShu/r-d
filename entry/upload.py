98e3184acf7a35266c126cfb91bd894d7721c462
ShuEzaki <ezashu0227@gmail.com>
2021-11-30 21:54:56 +0900

    fix return

--- entry/upload.py
+++ entry/upload.py
@@ -27,10 +27,10 @@ def uploads_file():
         "data":img_b
     }
     response = requests.post(url,data=img_data)
-    #return jsonify(response.json())
+    
     img = '<img src="data:image/png;base64,' + response.json()['data'] + '"/>'
-    time = response.json()['time']
-    return time
+    #time = response.json()['time']
+    return response.json()['time']
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=8080,threaded=True)
