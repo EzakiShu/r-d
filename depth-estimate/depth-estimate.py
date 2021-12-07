4acd3fb567a618d76ad136608c0e35dc95d4b426
munvei <roots19981108@gmail.com>
2021-12-07 16:36:09 +0900

    深度推定をプロセスを作って行うようにした

--- depth-estimate/depth-estimate.py
+++ depth-estimate/depth-estimate.py
@@ -7,12 +7,32 @@ from io import BytesIO
 import cv2
 import json
 from test_split import load_model, est_image
+from multiprocessing import Process,Pipe
 
 app     = Flask(__name__)
 device  = None
 feed    = []
 encoder = None
 depth_decoder = None
+
+def estimateFunc(pipe, image):
+  # OpenCV -> Pillow (narray to numpy)
+  image_pil = Image.fromarray(image)
+  
+  # depth estimate
+  global device, feed, encoder, depth_decoder
+  depth_image_pil = est_image(image_pil, device, feed, encoder, depth_decoder)
+
+  # Pillow -> OpenCV
+  depth_image_bgr = np.array(depth_image_pil)
+  depth_image = cv2.cvtColor(depth_image_bgr, cv2.COLOR_BGR2RGB) 
+  
+  # バイナリに変換
+  image_b = i2b(depth_image)
+
+  # 結果を pipe で渡す
+  pipe.send(image_b)
+ 
 
 @app.route('/')
 def index():
@@ -27,24 +47,19 @@ def estimate():
   # POSTされたファイルをOpenCVに変換
   file_data = request.files['file'].read()
   image_np = np.fromstring(file_data, np.uint8)
-  image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-  
-  # OpenCV -> Pillow
-  image_pil = Image.fromarray(image)
-  
-  # depth estimate
-  global device, feed, encoder, depth_decoder
-  depth_image_pil = est_image(image_pil, device, feed, encoder, depth_decoder)
+  image = cv2.imdecode(image_np, cv2.IMREAD_COLOR) # opencv image (narray)
 
-  # Pillow -> OpenCV
-  depth_image_bgr = np.array(depth_image_pil)
-  depth_image = cv2.cvtColor(depth_image_bgr, cv2.COLOR_BGR2RGB) 
-  
-  # バイナリに変換
-  image_b = i2b(depth_image)
+  # make pipe and make process
+  read_pipe, write_pipe = Pipe()
+  process = Process(target=estimateFunc, args=(write_pipe, image,))
+  write_pipe.close()
+  process.start()
+  res_image = read_pipe.recv()
+  read_pipe.close()
+  process.join()
+  return render_template('upload.html', image=res_image)
 
-  return render_template('upload.html', image=image_b)
-        
+       
 if __name__ == "__main__":
   
   art_args = type("hoge", (object,), {
