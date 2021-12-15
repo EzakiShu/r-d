24ea1d3889674661d3665217e3ab6e086025c1be
ShuEzaki <ezashu0227@gmail.com>
2021-12-15 16:04:44 +0900

    add get time

--- entry/upload.py
+++ entry/upload.py
@@ -1,37 +1,52 @@
-import os, json, sys, requests, cv2
+import requests
+import cv2
 import numpy as np
-from flask import Flask, request, redirect, url_for,jsonify
-from flask import send_from_directory, render_template
+from flask import Flask, request
+from flask import render_template
 from werkzeug.utils import secure_filename
-import random, string
 from converter import i2b, b2i
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('upload.html')
 
+
 @app.route('/', methods=['POST'])
 def uploads_file():
-    #opencvでPOSTされたファイルを読み込む
+    # opencvでPOSTされたファイルを読み込む
     file_data = request.files['file'].read()
     nparr = np.fromstring(file_data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    #バイナリに変換
+    # バイナリに変換
     img_b = i2b(img)
 
-    #画像の送信
-    url = "http://python-detection1:8080/api/predict"
-    img_data = {
-        "data":img_b
+    # 時間を取得
+    url = "http://time_data:10000/get-time"
+    time_data = requests.post(url, data=0)
+    time_data = {
+        "detection1": float(time_data.json()["detection1"]),
+        "detection2": float(time_data.json()["detection2"]),
+        "detection3": float(time_data.json()["detection3"])
     }
-    response = requests.post(url,data=img_data)
-    #print(response.json().keys())
+
+    # 直前の実行時間が最短なPod
+    min_time_edge = min(time_data.items(), key=lambda x: x[1])[0]
+
+    # 画像の送信
+    url = "http://python-" + min_time_edge + ":8080/api/predict"
+    img_data = {
+        "data": img_b
+    }
+    response = requests.post(url, data=img_data)
+
     img = '<img src="data:image/png;base64,' + response.json()['data'] + '"/>'
     #time = response.json()['time']
-    #return str(time)
-    return img 
-    
+    # return str(time)
+    return img
+
+
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8080,threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=8080, threaded=True)
