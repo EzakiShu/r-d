219037f60fd3ec9700665aa293820549b451a46c
ShuEzaki <ezashu0227@gmail.com>
2021-12-16 08:11:08 +0900

    fix sql

--- people_detection/people_detection.py
+++ people_detection/people_detection.py
@@ -69,7 +69,7 @@ def predict():
         # 実行時間の計算
         end = time.time() - start
         cursor = conn.cursor()
-        sql = "UPDATE detection SET time=15 WHERE pod='detection1'"
+        sql = ("UPDATE detection SET time=15 WHERE pod='detection1'")
         # sql = "UPDATE detection SET time=" + \
         #   str(end) + " WHERE pod='detection1'"
         cursor.execute(sql)
