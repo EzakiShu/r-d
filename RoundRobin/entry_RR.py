0ce0cfa23d8e799b8233c6d831fe17cbb1ac671a
soft-g10 <kut.group10@gmail.com>
2022-01-18 19:56:10 +0900

    make RoundRobin

---
+++ RoundRobin/entry_RR.py
@@ -0,0 +1,78 @@
+import requests
+import cv2
+import logging
+import numpy as np
+import mysql.connector
+from flask import Flask, request
+from flask import render_template
+from werkzeug.utils import secure_filename
+from converter import i2b, b2i
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+
+@app.route('/', methods=['POST'])
+def uploads_file():
+    # opencvでPOSTされたファイルを読み込む
+    file_data = request.files['file'].read()
+    nparr = np.fromstring(file_data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    # バイナリに変換
+    img_b = i2b(img)
+
+    # database接続
+    conn = mysql.connector.connect(
+        host='mysql-server',
+        port='3306',
+        user='devuser',
+        password='devuser',
+        database='time'
+    )
+    cursor = conn.cursor()
+    sql = ("SELECT next FROM RoundRobin")
+    cursor.execute(sql)
+    next_edge = cursor.fetchone()
+
+    # 画像の送信
+    url = "http://python-detection" + next_edge[0] + ":8080/api/predict"
+    img_data = {
+        "data": img_b
+    }
+
+    # 転送時間計測
+    #start = time.time()
+
+    # 検知リクエスト
+    response = requests.post(url, data=img_data)
+
+    # 転送時間計測
+    #end = time.time() - start
+    #transfer_time = end - response.json()['time']
+
+    # next pod
+    if next_edge[0] == 1:
+        next_edge[0] += 1
+    elif next_edge[0] == 2:
+        next_edge[0] += 1
+    elif next_edge[0] == 3:
+        next_edge[0] = 1
+    sql = "UPDATE next SET " + next_edge[0]
+    cursor.execute(sql)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    img = '<img src="data:image/png;base64,' + response.json()['data'] + '"/>'
+    #time = response.json()['time']
+    # return str(time)
+    return img
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=8080, threaded=True)
