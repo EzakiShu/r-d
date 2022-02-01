9d4de7b0b1f853e4f312c34b6794c143cda5a413
soft-g10 <kut.group10@gmail.com>
2022-02-02 03:19:52 +0900

    add second select

--- teian/upload.py
+++ teian/upload.py
@@ -12,6 +12,8 @@ import time
 app = Flask(__name__)
 
 count = 0
+pre_select_edge1 = 0
+pre_select_edge2 = 0
 
 
 @app.route('/')
@@ -26,7 +28,7 @@ def uploads_file():
 
     global count
     count += 1
-    print("")
+
     # opencvでPOSTされたファイルを読み込む
     file_data = request.files['file'].read()
     nparr = np.fromstring(file_data, np.uint8)
@@ -47,12 +49,21 @@ def uploads_file():
         database='time'
     )
     cursor = conn.cursor(buffered=True)
-    sql = ("SELECT pod FROM detection WHERE time=(SELECT MIN(time) FROM detection)")
+    sql = ("SELECT pod FROM detection")
     cursor.execute(sql)
-    min_time_edge1 = cursor.fetchone()
+    min_time_edge1 = cursor.fetchall()
+    select_edge1 = min(
+        min_time_edge1[0][1], min_time_edge1[1][1], min_time_edge1[2][1])
+
+    global pre_select_edge1
+    if pre_select_edge1 == select_edge1:
+        select_edge1 = sorted(
+            min_time_edge1[0][1], min_time_edge1[1][1], min_time_edge1[2][1])[2]
+
+    pre_select_edge1 = select_edge1
 
     # 画像の送信
-    url = "http://python-" + min_time_edge1[0] + ":8080/api/predict"
+    url = "http://python-detection" + select_edge1 + ":8080/api/predict"
     img_data = {
         "data": img_b
     }
@@ -70,14 +81,23 @@ def uploads_file():
 
     # DB更新
     sql = "UPDATE detection SET time=" + \
-        str(detection) + "WHERE pod='" + min_time_edge1[0] + "'"
+        str(detection) + "WHERE pod=detection'" + select_edge1 + "'"
     cursor.execute(sql)
 
-    sql = ("SELECT pod FROM depth WHERE time=(SELECT MIN(time) FROM depth)")
+    sql = ("SELECT pod FROM depth")
     cursor.execute(sql)
-    min_time_edge2 = cursor.fetchone()
+    min_time_edge2 = cursor.fetchall()
 
-    url = "http://python-" + min_time_edge2[0] + ":8080/depth"
+    select_edge2 = min(
+        min_time_edge2[0][1], min_time_edge2[1][1], min_time_edge2[2][1])
+
+    global pre_select_edge2
+    if pre_select_edge2 == select_edge2:
+        select_edge2 = sorted(
+            min_time_edge2[0][1], min_time_edge2[1][1], min_time_edge2[2][1])[2]
+
+    pre_select_edge2 = select_edge2
+    url = "http://python-depth" + select_edge2 + ":8080/depth"
 
     # 実行時間計測
     depth = time.time()
@@ -91,7 +111,7 @@ def uploads_file():
 
     # DB更新
     sql = "UPDATE depth SET time=" + \
-        str(depth) + "WHERE pod='" + min_time_edge2[0] + "'"
+        str(depth) + "WHERE pod=depth'" + select_edge2 + "'"
     cursor.execute(sql)
 
     cursor.close()
@@ -100,9 +120,9 @@ def uploads_file():
 
     all_time = time.time() - all_time
     time_data = {
-        "detection_pod": min_time_edge1[0],
+        "detection_pod": select_edge1,
         "detection_time": detection,
-        "depth_pod": min_time_edge2[0],
+        "depth_pod": select_edge2,
         "depth_time": depth,
         "exec": all_time,
         "count": count
@@ -111,4 +131,4 @@ def uploads_file():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=8080, threaded=False)
