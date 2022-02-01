6c26828e6d81576640259be8d502cb2783cba2ee
soft-g10 <kut.group10@gmail.com>
2022-02-01 22:57:57 +0900

    no thread

--- hoge/upload.py
+++ hoge/upload.py
@@ -105,4 +105,4 @@ def uploads_file():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=8080, threaded=False)
